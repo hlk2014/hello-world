3ce175a0dd7dd2829a7eec65255d963b9fbd5717
Your Name <you@example.com>
2019-01-02 14:43:26 +0000

    add some sh files for automating emacs/git/bash setup

---
+++ copy_bashrc_emacs_init_in.sh
@@ -0,0 +1,7 @@
+echo "Copying ~/.bashrc to dot_bashrc.txt"
+
+cp ~/.bashrc dot_bashrc.txt  -f
+
+echo "Copying ~/.emacs.d/init.el dot_init_el.txt"
+
+cp ~/.emacs.d/init.el dot_init_el.txt  -f
---
+++ copy_bashrc_emacs_init_out.sh
@@ -0,0 +1,7 @@
+echo "Copying dot_bashrc.txt to ~/.bashrc"
+
+cp dot_bashrc.txt ~/.bashrc -f
+
+echo "Copying dot_init_el.txt to ~/.emacs.d/init.el"
+
+cp dot_init_el.txt ~/.emacs.d/init.el -f
